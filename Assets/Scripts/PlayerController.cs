7c3c3f9e22f4831bf8b8b69216e09b0d51c74068
Mr. Satan <0421jamesheo@gmail.com>
2020-01-29 23:25:36 -0500

    Added Collision to Player Controller and Pick Up Tag
    Changed the PlayerController script so that object collision could be added and added, as well as applied, the Pick Up tag that will be utilized to be recognized for the collisions.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,4 +23,13 @@ public class PlayerController : MonoBehaviour
 
 	rb.AddForce(movement * speed);
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+	if (other.gameObject.CompareTg("Pick Up"))
+	{
+		other.gameObject.SetActive(false);
+	}
+     }
+
 }
